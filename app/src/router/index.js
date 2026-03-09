3d8f3f08bb3582734bf29f1bf0a0b6595fa94825
joe052 <joekaranjasenior52@gmail.com>
2026-03-09 20:44:32 +0300

    message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -6,32 +6,59 @@ import { useAuthStore } from "../store";
 const routes = [
   { path: "/", redirect: { name: "order" } },
   { path: "/order", name: "order", component: OrderView },
-  { path: "/dashboard", name: "dashboard", component: DashboardView },
+  { path: "/confirm-order", name: "confirm-order", component: ConfirmOrder },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
   {
     path: "/order/:id/pay",
     name: "payment",
     component: PaymentView,
     props: true, // Allows the :id to be passed as a prop to the component
+    meta: { requiresAuth: true },
   },
-  { path: "/confirm-order", name: "confirm-order", component: ConfirmOrder },
 ];
 
 /**Initialize here */
 const router = createRouter({ history: createWebHistory(), routes });
 
-// router.beforeEach(async (to, from) => {
-//   // redirect to login page if not logged in and trying to access a restricted page
-//   const publicPages = ["/", "/login"];
-//   const authRequired = !publicPages.includes(to.path);
-//   const auth = useAuthStore();
+router.beforeEach(async (to) => {
+  const auth = useAuthStore();
 
-//   /**add function to check if user is logged in */
-//   if (authRequired && !auth.user) {
-//     auth.returnUrl = to.fullPath;
-//     auth.isLoginModalOpen = true;
-//     return from.fullPath;
-//     // return "/login";
-//   }
-// });
+  /**If the user is just landing, try to restore session first
+   * This "picks up" the session from the Magic Link URL hash
+   */
+  if (!auth.user) {
+    try {
+      await auth.fetchUser();
+    } catch (err) {
+      console.error("Failed to restore session:", err);
+    }
+  }
+
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  // 2. If route requires auth and we still don't have a user
+  if (requiresAuth && !auth.isLoggedIn && auth.profile?.role !== "ADMIN") {
+    console.warn("Non-admin tried to access admin dashboard");
+    await auth.logout();
+    return {
+      path: "/login",
+      query: { returnUrl: to.fullPath },
+    };
+  }
+
+  /**If user is logged in and tries to go to login page */
+  if (
+    (to.name === "login" || to.name === "signup") &&
+    auth.isLoggedIn &&
+    auth.profile?.role === "ADMIN"
+  ) {
+    return { path: "/" };
+  }
+});
 
 export default router;
