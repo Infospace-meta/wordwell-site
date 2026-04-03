5d6f3d9c05c810683d0ac532b23f5afe32784b03
Immanuel <immanuel4082@gmail.com>
2026-04-04 02:31:57 +0300

    message

--- app/tailwind.config.mjs
+++ app/tailwind.config.mjs
@@ -1,8 +1,12 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+        fontFamily: {
+            sans: ['Instrument sans', 'sans-serif'],
+        }
+    },
+  },
+  plugins: [],
+};
