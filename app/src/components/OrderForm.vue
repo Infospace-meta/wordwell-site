dfa890b59163f99fb61705355f0008c725a1d6cf
joe052 <joekaranjasenior52@gmail.com>
2026-04-11 06:37:21 +0300

    message

--- app/src/components/OrderForm.vue
+++ app/src/components/OrderForm.vue
@@ -360,6 +360,27 @@
                   ⚠️ Please upload at least one attachment.
                 </p>
 
+                <!-- FILE ERROR MESSAGE -->
+                <p
+                  v-if="fileErrorMessage"
+                  class="text-red-500 text-xs mt-2 bg-red-50 p-2 rounded border border-red-100 flex items-center gap-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-3 w-3"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  {{ fileErrorMessage }}
+                </p>
+
+                <!-- Uploading text -->
                 <p
                   v-if="uploading"
                   class="text-blue-500 text-xs mt-2 animate-pulse font-bold"
