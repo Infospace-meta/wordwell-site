1fc0011ec184c5a8dce719f192df65c18ebf07c5
joe052 <joekaranjasenior52@gmail.com>
2026-06-18 14:55:14 +0300

    updated services pricing

--- app/src/config/constants.js
+++ app/src/config/constants.js
@@ -2,11 +2,20 @@ export const ROOT_URL = "http://localhost:3000";
 
 /**Order service costs */
 export const SERVICE_COSTS = {
-  "essay writing": 10,
-  "assignment help": 8,
-  "research paper": 10,
-  "thesis/dissertation": 15,
-  "editing and proofreading": 6,
-  "literature review": 10,
-  "presentation and reports": 5,
+  "essay writing": 13,
+  "assignment help": 11,
+  "research paper": 13,
+  "thesis/dissertation": 20,
+  "editing and proofreading": 8,
+  "literature review": 13,
+  "presentation and reports": 7,
 };
+
+/**OLDER PRICING - INFLATED WITH 33% TAX */
+// "essay writing": 10,
+// "assignment help": 8,
+// "research paper": 10,
+// "thesis/dissertation": 15,
+// "editing and proofreading": 6,
+// "literature review": 10,
+// "presentation and reports": 5,
